e68a3cad7ec8563d43ee1586c6644fecd0474a81
Freddy May <freddymay@mac.com>
2023-04-10 09:53:41 +0100

    boundaries

--- src/main/java/util/Trivialissimo.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Trivialissimo {
-
-    public static int parseInt(String value) throws IllegalArgumentException {
-        return Integer.parseInt(value);
-    }
-
-}
---
+++ src/main/java/util/boundary/Boundary.java
@@ -0,0 +1,14 @@
+package util.boundary;
+
+public class Boundary {
+    public static boolean isInsurable(int age) {
+        return false;
+    }
+
+//    // After refactoring
+//    public static boolean isInsurable(int age) {
+//        if(age < 18 || age >25)
+//            return false;
+//        return true;
+//    }
+}
