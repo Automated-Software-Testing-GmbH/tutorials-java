e85a5fee11749bb589139ad9c5c9bc047832be47
Freddy May <freddymay@mac.com>
2023-04-12 18:33:39 +0100

    base

--- src/main/java/util/Abc.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Abc {
-    public static int testMe(int n) {
-        return n + 1;
-    }
-}
--- src/main/java/util/trivialissimo/Trivialissimo.java
+++ src/main/java/util/trivialissimo/Trivialissimo.java
@@ -7,4 +7,11 @@ public class Trivialissimo {
         return Integer.parseInt(value);
     }
 
+//    // Refactored code to fix the "0" test case
+//    public static int parsePositiveIntString(String value) throws Exception {
+//        int convertedValue = Integer.parseInt(value);
+//        if(convertedValue == 0) throw new Exception("0 cannot be accepted");
+//        return convertedValue;
+//    }
+
 }
