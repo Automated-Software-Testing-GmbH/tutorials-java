857a2387f2937014c59478ef2d42967342cb1612
Freddy May <freddymay@mac.com>
2023-04-14 11:55:47 +0100

    bug

--- src/main/java/util/sideeffects/SideEffect.java
+++ src/main/java/util/sideeffects/SideEffect.java
@@ -2,6 +2,14 @@ package util.sideeffects;
 
 public class SideEffect {
 
+    private int steps;
 
+    public SideEffect(int initialSteps) {
+        this.steps = initialSteps;
+    }
+
+    public void addSteps(int steps) {
+        this.steps += steps;
+    }
 
 }
