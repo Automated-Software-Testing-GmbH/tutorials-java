322319f7fa0ccf2e00369d079da7db8b62247525
Freddy May <freddymay@mac.com>
2023-04-15 14:46:03 +0100

    chaining 50%; about to do mocks

---
+++ src/main/java/util/chaining/Chain.java
@@ -0,0 +1,10 @@
+package util.chaining;
+
+public class Chain {
+    static int multiplyPositiveByTen(int input) throws Exception {
+        if(input <= 0) throw new Exception("Input must be greater than or equal to 1");
+        return input * 10;
+    }
+}
+
+
--- src/main/java/util/exceptions/ExceptionDemo.java
+++ src/main/java/util/exceptions/ExceptionDemo.java
@@ -1,8 +1,8 @@
 package util.exceptions;
 
 public class ExceptionDemo {
-    public static double generalException(int number, int divisor) {
-        return (float) number / divisor;
+    public static int generalException(int number, int divisor) {
+        return number / divisor;
     }
     public static double userException(int number, int divisor) throws DivideByZeroException {
         if(divisor == 0) throw new DivideByZeroException("Divided " + number + " by " + divisor);
@@ -15,3 +15,5 @@ class DivideByZeroException extends Exception {
         super(errorMessage);
     }
 }
+
+
