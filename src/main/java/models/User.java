57f36e359e53f6809a29d404b025a390b8e0e09c
Freddy May <freddymay@mac.com>
2023-04-15 06:16:20 +0100

    complex args, side effects updated

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class User {
@@ -56,5 +57,18 @@ public class User {
 
     public void setBio(String bio) {
         this.bio = bio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(age, user.age) && Objects.equals(bio, user.bio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, bio);
     }
 }
--- src/main/java/util/complexargs/ComplexArgs.java
+++ src/main/java/util/complexargs/ComplexArgs.java
@@ -3,11 +3,18 @@ package util.complexargs;
 import models.User;
 
 public class ComplexArgs {
-    static User setEmptyBioString(User user) {
+
+    static User setDefaultBioString(User user) {
         if(user.getBio().trim().equals("")) {
             user.setBio("No bio available");
         }
         return user;
     }
+
+    static User deleteBioString(User user) {
+        user.setBio("");
+        return user;
+    }
+
 }
 
