ca6256ec698b9590d9b1b4cbd679da5d921157b1
Freddy May <freddymay@mac.com>
2023-04-25 12:23:20 +0100

    commented code for Trvialissimo

--- src/main/java/util/trivialissimo/Trivialissimo.java
+++ src/main/java/util/trivialissimo/Trivialissimo.java
@@ -4,10 +4,16 @@ public class Trivialissimo {
 
     public static int parsePositiveIntString(String value) throws Exception {
         int convertedValue = Integer.parseInt(value);
-        if(convertedValue == 0) throw new Exception("0 cannot be accepted");
-
         return convertedValue;
     }
+
+}
+
+//    // Basic method without the validation check for "0"
+//    public static int parsePositiveIntString(String value) throws Exception {
+//        int convertedValue = Integer.parseInt(value);
+//        return convertedValue;
+//    }
 
 //    // Refactored code to fix the "0" test case
 //    public static int parsePositiveIntString(String value) throws Exception {
@@ -16,4 +22,7 @@ public class Trivialissimo {
 //        return convertedValue;
 //    }
 
-}
+//    // Basic method for TDD approach
+//    public static int parsePositiveIntString(String value) throws Exception {
+//        return 1;
+//    }
